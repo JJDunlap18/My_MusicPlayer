c4de3b8f358897d906d6a7c6ecbafcb8dbcf5754
jjdun <jjdunlap37@gmail.com>
2021-09-14 17:20:15 -0400

    added the menus to the root window

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -16,7 +16,7 @@ class MusicPlayer:
         root.geometry('540x450')
         root.resizable(False, False)
         root.title('JD Music Player')
-
+#
         # Creating the main frame for the music player
         player_frame = Frame(root)
         player_frame.pack()
@@ -24,40 +24,46 @@ class MusicPlayer:
         # Creating the directory for the songs
         song_playlist = Listbox(player_frame, bg='white', fg='black', width=80, selectbackground='blue', selectforeground='black')
         song_playlist.grid(row=0, column=0)
+#
+        # Creating the main menu on the root window
+        song_menu = Menu(root)
+        root.config(menu=song_menu)  # updates the root window to have a menu option
 
-        # Creating the menu/options for adding and removing songs from the playlist
-        add_song_menu = Menu(root)
-        root.config(menu=add_song_menu)  # updates the root window to have a menu option
-        add_song_menu.add_cascade(label='Add Songs')  # adds a menu called 'Add Songs' to the root window
+        # Creating a menu for adding songs to the playlist
+        add_song_menu = Menu(song_menu)
+        song_menu.add_cascade(label='Add Songs', menu=add_song_menu)  # adds a menu called 'Add Songs' to the root window
         add_song_menu.add_command(label='Add Songs to Playlist', command=self.add_songs)  # creates an option to add songs under the 'Add Songs' menu
 
-
+        # Creating a menu for removing songs from the playlist
+        remove_song_menu = Menu(root)
+        song_menu.add_cascade(label='Remove Songs', menu=remove_song_menu)  # adds a menu called 'Remove Songs' to the root window
+        remove_song_menu.add_command(label='Remove Song from Playlist', command=self.remove_song)  # removes the selected song from the playlist
+        remove_song_menu.add_command(label='Remove All Songs from Playlist', command=self.remove_all_songs)  # removes all songs from the playlist
 
         # Creating the frame for the buttons
         controls_frame = Frame(player_frame)
         controls_frame.grid(row=2, column=0, pady=10)
-
+#
         # Images for the play, stop, pause, rewind, and forward buttons
         back_img = PhotoImage(file='Button_Images/backward.png')
         forward_img = PhotoImage(file='Button_Images/forward.png')
         play_img = PhotoImage(file='Button_Images/play.png')
         pause_img = PhotoImage(file='Button_Images/pause.png')
         stop_img = PhotoImage(file='Button_Images/stop.png')
-
+#
         # Creating buttons for the player
         play_button = Button(controls_frame, image=play_img, borderwidth=0, command=self.play_music)
         stop_button = Button(controls_frame, image=stop_img, borderwidth=0, command=self.stop_music)
         pause_button = Button(controls_frame, image=pause_img, borderwidth=0, command=self.pause_music)
         back_button = Button(controls_frame, image=back_img, borderwidth=0, command=self.back_music)
         forward_button = Button(controls_frame, image=forward_img, borderwidth=0, command=self.next_song)
-
+#
         # Placing buttons on the controls_frame
         back_button.grid(row=0, column=0, padx=5)
         forward_button.grid(row=0, column=1, padx=5)
         play_button.grid(row=0, column=2, padx=5)
         pause_button.grid(row=0, column=3, padx=5)
         stop_button.grid(row=0, column=4, padx=5)
-
 
         self.stop_playing = False  # used to track when the status bar for the songs should stop updating
         self.play_state = False  # used to track when the song is in a paused or un-paused state
