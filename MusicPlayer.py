4ac3dbebd0724de925f5a25dcdc7caabd10fc133
jjdun <jjdunlap37@gmail.com>
2021-09-14 13:03:51 -0400

    changing the overall approach to the music player

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,17 +1,41 @@
 from tkinter import *
-from PIL import ImageTk, Image
 from pygame import mixer
 from tkinter import filedialog
+import time
+from mutagen.mp3 import MP3
+from mutagen.wave import WAVE
+import tkinter.ttk as ttk
 
 
 class MusicPlayer:
 
     def __init__(self):
 
+        # Creating the initial window for the music player
         root = Tk()
-        root.geometry('540x400')
+        root.geometry('540x450')
         root.resizable(False, False)
         root.title('JD Music Player')
+
+        # Creating the main frame for the music player
+        player_frame = Frame(root)
+        player_frame.pack()
+
+        # Creating the directory for the songs
+        song_playlist = Listbox(player_frame, bg='white', fg='black', width=80, selectbackground='blue', selectforeground='black')
+        song_playlist.grid(row=0, column=0)
+
+        # Creating the menu/options for adding and removing songs from the playlist
+        add_song_menu = Menu(root)
+        root.config(menu=add_song_menu)  # updates the root window to have a menu option
+        add_song_menu.add_cascade(label='Add Songs')  # adds a menu called 'Add Songs' to the root window
+        add_song_menu.add_command(label='Add Songs to Playlist', command=self.add_songs)  # creates an option to add songs under the 'Add Songs' menu
+
+
+
+        # Creating the frame for the buttons
+        controls_frame = Frame(player_frame)
+        controls_frame.grid(row=2, column=0, pady=10)
 
         # Images for the play, stop, pause, rewind, and forward buttons
         back_img = PhotoImage(file='Button_Images/backward.png')
@@ -20,62 +44,57 @@ class MusicPlayer:
         pause_img = PhotoImage(file='Button_Images/pause.png')
         stop_img = PhotoImage(file='Button_Images/stop.png')
 
+        # Creating buttons for the player
+        play_button = Button(controls_frame, image=play_img, borderwidth=0, command=self.play_music)
+        stop_button = Button(controls_frame, image=stop_img, borderwidth=0, command=self.stop_music)
+        pause_button = Button(controls_frame, image=pause_img, borderwidth=0, command=self.pause_music)
+        back_button = Button(controls_frame, image=back_img, borderwidth=0, command=self.back_music)
+        forward_button = Button(controls_frame, image=forward_img, borderwidth=0, command=self.next_song)
 
-        # Creating the buttons for the music player
-        controls_frame = Frame(root)
-        controls_frame.pack(side=BOTTOM, fill='y')
+        # Placing buttons on the controls_frame
+        back_button.grid(row=0, column=0, padx=5)
+        forward_button.grid(row=0, column=1, padx=5)
+        play_button.grid(row=0, column=2, padx=5)
+        pause_button.grid(row=0, column=3, padx=5)
+        stop_button.grid(row=0, column=4, padx=5)
 
 
-        load_button = Button(controls_frame, text='Load', width=5, font=('Times', 10), command=self.load_music)
-        # width controls the size of the button itself, font controls the font of the button and the size of the text
-        # try using an image= within the Button function to add the symbols instead of text for each button
-        play_button = Button(controls_frame, image=play_img, width=5, font=('Times', 10), command=self.play_music)
-        stop_button = Button(controls_frame, image=stop_img, width=5, font=('Times', 10), command=self.stop_music)
-        pause_button = Button(controls_frame, image=pause_img, width=5, font=('Times', 10), command=self.pause_music)
-        back_button = Button(controls_frame, image=back_img, width=5, font=('Times',10), command=self.rewind_music)
-        forward_button = Button(controls_frame, image=forward_img, width=5, font=('Times',10), command=self.next_song)
+        self.stop_playing = False  # used to track when the status bar for the songs should stop updating
+        self.play_state = False  # used to track when the song is in a paused or un-paused state
+        self.play_time_var = False  # used to track when play_time is running so play_time2 can start
+        self.song_length = None  # used to track the length of each song for the status bar
 
-        # Placing the buttons onto the root window
-        load_button.grid(row=0, column=0, columnspan=4)
-        play_button.grid(row=0, column=1, columnspan=4)
-        # play_button.place(x=110, y=20)
-        # pause_button.place(x=220, y=20)
-        # rewind_button.place(x=0, y=60)
-        # stop_button.place(x=110, y=60)
-        # next_button.place(x=220, y=60)
-
-        self.music_file = False  # Initializing this variable for later use. It is given a value of False but will be overwritten later
-        self.playing_state = False
-        # Might need to add music_file and playing_state as variables in __init__()
 
         root.mainloop()
 
-    def load_music(self):
-        self.music_file = filedialog.askopenfile()  # filedialog.askopenfile() opens a separate window so the user can select the directory to play music from
-        # Also consider filedialog.Directory()
-        print(self.music_file)
+    def add_songs(self):
+        pass
+
+    def remove_song(self):
+        pass
+
+    def remove_all_songs(self):
+        pass
+
+    def play_time(self):
+        pass
+
+    def play_time2(self):
+        pass
 
     def play_music(self):
-        if self.music_file:
-            mixer.init()
-            mixer.music.load(self.music_file)  # The file saved in the music_load function will now be loaded
-            mixer.music.play()  # Plays the file loaded in music.load()
+       pass
 
     def stop_music(self):
-        mixer.music.stop()  # Stops the music playback if it is playing. It won't unload the music
+        pass
 
     def pause_music(self):
-        if not self.playing_state:  # returns True if playing_state = False
-            mixer.music.pause()  # Pauses the music if playing_state is False
-            self.playing_state = True  # Turns playing_state True after the music is paused
-        else:
-            mixer.music.unpause()  # Unpauses the music if playing_state is True
-            self.playing_state = False  # changes playing_state back to False so it can be paused again
+        pass
 
-    def rewind_music(self):
-        mixer.music.rewind()
+    def back_music(self):
+        pass
 
-    def next_song(self):  # I may have to hard code this function in if pygame does not have a next function
+    def next_song(self):
         pass
 
 
